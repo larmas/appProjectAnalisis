840f11fd24599123c1477db701ee78daed5198d6
larmas <lucas.armas.7@gmail.com>
2016-06-27 10:22:11 -0300

    Graphic changes

--- models/product.js
+++ models/product.js
@@ -4,9 +4,14 @@ var Schema = mongoose.Schema;
 mongoose.connect("mongodb://localhost/appAnalisis");
 
 var product_schema = new Schema({
-  nameProduct: {type: String, required: "El nombre es obligatorio", maxlength: [60,"Nombre demasiado largo"], minlength: [4,"Nombre demasiado corto"]},
-  descriptionProduct: {type: String, required: "La descripcion es obligatoria"},
-  costProduct: {type: Number, min:[1, "El costo no puede ser menor a 1"]}
+  nameProduct: {type: String, 
+  				required: "El nombre es obligatorio", 
+  				maxlength: [60,"Nombre demasiado largo"], 
+  				minlength: [4,"Nombre demasiado corto"]},
+  descriptionProduct: {type: String, 
+  					   required: "La descripcion es obligatoria"},
+  costProduct: {type: Number, 
+  				min:[1, "El costo no puede ser menor a 1"]}
 });
 
 var Product = mongoose.model("Product", product_schema);
